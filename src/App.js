59d086adca4a4c85a592e348800573eccef071af
mykhailo-murat <mykhailo.murat@gmail.com>
2022-05-10 18:18:16 +0300

    fix decimal

--- src/App.js
+++ src/App.js
@@ -133,6 +133,7 @@ function formatOperand(operand) {
     if (operand == null) return
     const [integer, decimal] = operand.split('.')
     if (decimal == null) return INTEGER_FORMATTER.format(integer)
+    return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
 }
 
 
